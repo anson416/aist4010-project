a1b5fc5866e0bbb9668423ca660c408b903db346
anson416 <lamyiufung2003@gmail.com>
2024-03-13 11:40:42 +0800

    Add ChannelModification

--- model/utils.py
+++ model/utils.py
@@ -18,6 +18,38 @@ class LayerNorm2d(nn.LayerNorm):
         return x
 
 
+class ChannelModification(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+    ) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.modification = (
+            nn.Sequential(
+                LayerNorm2d(in_channels, eps=1e-6),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1),
+            )
+            if in_channels != out_channels
+            else nn.Identity()
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.modification(x)
+
+
+class Concatenation(nn.Module):
+    def __init__(self, dim: int = 0) -> None:
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, *inputs: Tensor) -> Tensor:
+        return torch.cat(inputs, dim=self.dim)
+
+
 # Global Response Normalization
 class GRN(nn.Module):
     """
